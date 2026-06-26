22fd067dadfadef3f53afaa3eba272f7393c16d7
RaykoTL <137858607+RaykoTL@users.noreply.github.com>
2026-06-26 12:14:30 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from "react";
-const fondoHero = getAssetUrl("fondo-hero (1).jpg");
-
-
+import fondoHero from "./assets/fondo-hero (1).jpg";
 const style = `
   @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&family=Poppins:wght@400;600;700;800&display=swap');
 
